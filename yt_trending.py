13800c7a4f599e6ac2d72b5f4994eaaaccd5beb7
nb5hd <nb5hd@virginia.edu>
2017-06-19 09:44:21 -0400

    Removed old yt_trending script

--- yt_trending.py
+++ yt_trending.py
@@ -1,88 +1,177 @@
+# Nikhil Bhaip
+# yt_trending_v2.py
+# TODO: Use File I/O and LOG info
+# TODO: Rigorous Unit Tests
+
 import requests
 from bs4 import BeautifulSoup
 import psycopg2
 
+# "Creator on The Rise" does not count as trending, so I will exclude these videos
+# Their ordinal positions are enumerated below
+SKIP_ROW_NUMS = [4, 5, 6, 7, 8, 9]
 
-page = requests.get("https://www.youtube.com/feed/trending")
-soup = BeautifulSoup(page.content, 'html.parser')
+YOUTUBE_URL = "https://www.youtube.com/feed/trending"
+PARSER = "html.parser"
+HOST = "localhost"
+PORT = 5432
 
-view_rows = soup.find_all(class_="yt-lockup-meta-info")
-title_rows = soup.find_all(class_="yt-lockup-title")
+TABLE_EXISTS_QUERY = "SELECT EXISTS(SELECT * FROM information_schema.tables where table_name=%s)"
+YT_TABLE = 'yt_table'
+CREATE_TABLE_QUERY = """CREATE TABLE yt_table(
+          title TEXT,
+          views INTEGER,
+          duration TEXT
+          )"""
+INSERT_ROW_QUERY = "INSERT INTO yt_table (title, views, duration) VALUES (%s, %s, %s);"
 
-view_list = []
-title_list = []
-time_list = []
 
-# Clean up views!
-for i, row in enumerate(view_rows):
-    # "Creator on The Rise" does not count as trending, so I will exclude these videos
-    # They are identified in the code by having the word "views" come before the word "ago" in each row
-    row_text = view_rows[i].get_text()
+# This class holds information about each video and a list of all_videos
+class Video:
+    # class variable "all_videos" holds all videos ever instantiated
+    all_videos = []
 
-    ago_index = row_text.index("ago")
-    views_index = row_text.index("views")
+    def __init__(self, title="", view=-1, duration=-1):
+        self.title = title
+        self.view = view
+        self.duration = duration
+        self.all_videos.append(self)
 
-    if views_index > ago_index:
-        view_string = row_text[(ago_index+3):(views_index-1)]
+    def __str__(self):
+        return "Title: " + self.title + "\nView: " + str(self.view) + "\nDuration: " + str(self.duration)
+
+
+class VideoListInfo:
+    view_list = []
+    title_list = []
+    duration_list = []
+
+    def __init__(self):
+        pass
+
+    # TODO Write general template code for add method
+    def add_view(self, view):
+        self.view_list.append(view)
+
+    def add_title(self, title):
+        self.title_list.append(title)
+
+    def add_duration(self, duration):
+        self.duration_list.append(duration)
+
+    # TODO Write general template code for converting 'n' lists into a video with 'n' params. (**kwargs maybe?)
+    def get_video_list(self):
+        if len(self.view_list) == len(self.title_list) and len(self.title_list) == len(self.duration_list):
+            video_list = []
+            for i in range(len(self.view_list)):
+                video = Video(self.title_list[i], self.view_list[i], self.duration_list[i])
+                video_list.append(video)
+            return video_list
+        else:
+            raise Exception("Error: Lengths of information lists don't match. \nExiting...")
+
+
+# TODO Gets page content from general URL (CHANGE URL procedures)
+def get_page_content():
+    page = requests.get(YOUTUBE_URL)
+    return BeautifulSoup(page.content, PARSER)
+
+# TODO create a class that holds video_list_info and case_info to avoid extraneous args in methods...
+
+
+def clean(row_text, video_list_info, case):
+    if case == "views":
+        ago_index = row_text.index("ago")
+        views_index = row_text.index("views")
+
+        view_string = row_text[(ago_index + 3):(views_index - 1)]
         view = view_string.replace(',', '')
 
-        view_list.append(view)
-
-# Clean up titles
-for i, row in enumerate(title_rows):
-
-    if i not in [4, 5, 6, 7, 8, 9]:
-        # print(row.get_text())
-
-        row_text = title_rows[i].get_text()
-
+        video_list_info.add_view(view)
+    elif case == "title_and_duration":
         duration_index = row_text.index("Duration")
 
-        title = row_text[:(duration_index-3)]
-        time = row_text[(duration_index+10):-1]
+        title = row_text[:(duration_index - 3)]
+        video_list_info.add_title(title)
 
-        title_list.append(title)
-        time_list.append(time)
+        duration = row_text[(duration_index+10):-1]
+        video_list_info.add_duration(duration)
+    else:
+        raise Exception("Invalid case entered")
 
-# print(len(view_list) == len(title_list))
 
-yt_data = list(zip(title_list, view_list, time_list))
-# _____________________________________________________________________________________________________________________
+def get_row_and_clean(rows, video_list_info, case="none"):
+    if not isinstance(video_list_info, VideoListInfo):
+        raise Exception("Invalid video_list_info type")
+    for i, row in enumerate(rows):
+        if i not in SKIP_ROW_NUMS:
+            row_text = row.get_text()
+            clean(row_text, video_list_info, case)
 
-conn = psycopg2.connect(database="nikhil", user="postgres", password="", host="localhost", port="5432")
 
-print("Opened database successfully")
+def get_database_info():
+    db = input("Enter database name: ")
+    uname = input("Enter username: ")
+    pword = input("Enter password: ")
+    return db, uname, pword
 
-cur = conn.cursor()
 
-# Check if yt_table already exists
-cur.execute("SELECT EXISTS(SELECT * FROM information_schema.tables where table_name=%s)",
- ('yt_table',))
-yt_table_exists = cur.fetchone()[0]
+# Returns true if YT_TABLE exists in the database
+def check_table_exists(cur):
+    cur.execute(TABLE_EXISTS_QUERY, (YT_TABLE,))
+    return cur.fetchone()[0]
 
-# If yt_table does not exist, then create a table and populate from CSV
-if(yt_table_exists != True):
-    cur.execute("""CREATE TABLE yt_table(
-      title TEXT,
-      views INTEGER,
-      duration TEXT
-      )""")
+
+def make_table(cur, yt_data):
+    cur.execute(CREATE_TABLE_QUERY)
 
     for video in yt_data:
-        video_title = video[0]
-        video_view = video[1]
-        video_time = video[2]
 
-        query = "INSERT INTO yt_table (title, views, duration) VALUES (%s, %s, %s);"
-        data = (video_title, video_view, video_time)
-
-        cur.execute(query, data)
+        data = (video.title, video.view, video.duration)
+        cur.execute(INSERT_ROW_QUERY, data)
 
     print("yt_table was successfully created and populated")
-else:
-    print("yt_table has already been created and populated")
 
 
-conn.commit()
-conn.close()
+def main():
+    content = get_page_content()
 
+    # Find all rows
+    view_rows = content.find_all(class_="yt-lockup-meta-info")
+    title_time_rows = content.find_all(class_="yt-lockup-title")
+
+    if len(view_rows) != len(title_time_rows):
+        raise Exception("Error: Mismatch between length of number of views and number of titles. \nExiting...")
+
+    video_list_info = VideoListInfo()
+
+    get_row_and_clean(view_rows, video_list_info, case="views")
+    get_row_and_clean(title_time_rows, video_list_info, case="title_and_duration")
+
+    # organize YouTube data into one list
+    yt_data = video_list_info.get_video_list()
+
+    # Connect to Database
+    # TODO Use File I/O to hold DB credentials
+    db, uname, pword = get_database_info()
+
+    # FIXME: Fix database authentication error handling
+    try:
+        conn = psycopg2.connect(database=db, user=uname, password=pword, host=HOST, port=PORT)
+    except psycopg2.Error:
+        raise Exception("Cannot connect to database.")
+
+    cur = conn.cursor()
+
+    table_exists = check_table_exists(cur)
+
+    if not table_exists:
+        make_table(cur, yt_data)
+    else:
+        print("yt_table has already been created and populated")
+
+    conn.commit()
+    conn.close()
+
+if __name__ == "__main__":
+    main()
